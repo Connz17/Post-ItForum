138370885b4100f5cb0664b76667c8b1b24904ce
Rimm Deres <rimmderes@gmail.com>
2023-04-25 15:43:42 +0100

    abandoned branch

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -15,7 +15,8 @@ const PostCard = ({title, date, author, comments, tags}) => {
     <div className='row'>
 
      <div className='postsContainer'>
-         <h3 className='title'>{title}</h3>
+         <h3 className='title'><a href=""> {title} </a></h3>
+         <p className='description'> Description: Lorem ipsum  dolor sit amet, consectetur adipiscing elit. Curabitur faucibus nulla magna, vitae rhoncus turpis vulputate et. Morbi condimentum sed purus vel maximus.  </p>
          <p className='date'>{date}</p>
          <p className='author'>{author}</p>
          <p className='comments'>{comments}</p>
