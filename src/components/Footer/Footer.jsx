94e09c6eae6a2a38444d291569228bc70c2ae08c
Connel Nwaodor <connel_n@hotmail.com>
2023-04-25 15:28:38 +0100

    took out wiki title and re-replaced footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,64 +5,37 @@ const Footer = () => {
 
 
   return (
+    // <>
+    //  <hr/>
+    //     <div class="footer-content">
+    //         <br></br>
+    //     <h3>MyMoon</h3>
+    //     {/* <h3>Follow us on social media!</h3> */}
+    //     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae felis scelerisque, gravida sapien non, cursus augue. Aenean id pretium turpis. Suspendisse eros nunc, sollicitudin nec.</p>
+    //     <ul class="socials">          
+    //          {/* <li><img src={instagramcopy} alt = "Productimg" width={50} /> </li>
+    //         <li><img src={snapchatcopy} alt = "Productimg" width={50}/></li>
+    //         <li><img src={tikkytokcopy} alt = "Productimg" width={50}/></li>
+    //         <li><img src={twittercopy} alt = "Productimg" width={50}/></li>
+    //         <li><img src={pinterestcopy} alt = "Productimg" width={50}/></li>  */}
+    //         {/* <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li> */}
+    //     </ul>
+    //     </div>
+    //     <div class="footer-bottom">
+    //     <p> &copy; <a href="/">PostIt</a> </p>
+    //     <div class="footer-menu">
+    //         <ul class="f-menu">
+    //         <li>Home</li>
+    //         <li>About</li>
+    //         <li>Contact</li>
+    //         </ul>
+    //     </div>
+    //     </div>
 
-    <>
-     <hr/>
-        
-
-        <div class="footer-content">
-
-            <br></br>
-
-
-        <h3>MyMoon</h3>
-        {/* <h3>Follow us on social media!</h3> */}
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae felis scelerisque, gravida sapien non, cursus augue. Aenean id pretium turpis. Suspendisse eros nunc, sollicitudin nec.</p>
-
-        <ul class="socials">
-
-             
-             {/* <li><img src={instagramcopy} alt = "Productimg" width={50} /> </li>
-
-            <li><img src={snapchatcopy} alt = "Productimg" width={50}/></li>
-
-            <li><img src={tikkytokcopy} alt = "Productimg" width={50}/></li>
-
-            <li><img src={twittercopy} alt = "Productimg" width={50}/></li>
-
-            <li><img src={pinterestcopy} alt = "Productimg" width={50}/></li>  */}
-
-            {/* <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li> */}
-
-        </ul>
-
-        </div>
-
-        <div class="footer-bottom">
-
-        <p> &copy; <a href="/">PostIt</a> </p>
-
-        <div class="footer-menu">
-
-            <ul class="f-menu">
-
-            <li>Home</li>
-
-            <li>About</li>
-
-            <li>Contact</li>
-
-            </ul>
-
-        </div>
-
-        </div>
-
-    </>
-    //  <div className="footer">
-    //     <p>Gul Ozdemir, Connel, Jason, Rimm 2023&copy;</p>
-    // </div>
+    // </>
+     <div className="footer">
+        <p>Gul Ozdemir, Connel, Jason, Rimm 2023&copy;</p>
+    </div>
   )
 }
 
