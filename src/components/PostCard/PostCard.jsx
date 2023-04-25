a085f2634938e5535534beed18f4bf297ff9a1c2
Rimm Deres <rimmderes@gmail.com>
2023-04-25 23:35:50 +0100

    finished posts

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -8,18 +8,20 @@ const PostCard = ({title, date, author, comments, tags}) => {
 
   return (
 <>
-    
+    {/* <h1>Posts</h1> */}
 
     <div className='container'>
 
     <div className='row'>
 
      <div className='postsContainer'>
-         <h3 className='title'><a href=""> {title} </a></h3>
+         <h3 className='title'> {title} 📝 </h3>
          <p className='description'> Description: Lorem ipsum  dolor sit amet, consectetur adipiscing elit. Curabitur faucibus nulla magna, vitae rhoncus turpis vulputate et. Morbi condimentum sed purus vel maximus.  </p>
          <p className='date'>{date}</p>
-         <p className='author'>{author}</p>
-         <p className='comments'>{comments}</p>
+         <p className='author'>👤 {author}</p>
+         <p className='more'>Read More...</p>
+         {/* ⬇️ */}
+         <p className='comments'>{comments} comments</p>
      </div>
      </div>
      </div>
